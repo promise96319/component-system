9042e70382fb1f90c07dd740f130fec0fa8f361b
Guanghui Qin <1248975357@qq.com>
2023-06-25 18:08:50 +0800

    feat: 目录调整

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
-import { UserModule } from './module/user/user.module';
-import { MajorVersionModule } from './module/major-version/major-version.module';
-import { ComponentModule } from './module/component/component.module';
-import { ApiDocModule } from './module/api-doc/api-doc.module';
+import { UserModule } from './modules/user/user.module';
+import { MajorVersionModule } from './modules/major-version/major-version.module';
+import { ComponentModule } from './modules/component/component.module';
+import { ApiDocModule } from './modules/api-doc/api-doc.module';
 
 @Module({
   imports: [
